2ad09d40506fd13eba69c591d8a192ab2b4999a3
Roman Arzumanyan <RomanArzumanyan@users.noreply.github.com>
2023-12-18 15:00:21 +0300

    Update conanfile.py
    Use default ffmpeg package settings except for shared libs

--- conanfile.py
+++ conanfile.py
@@ -11,21 +11,21 @@ class FfmpegRecipe(ConanFile):
     def configure(self):
         self.options["ffmpeg/*"].shared = True
 
-        self.options["ffmpeg/*"].with_xcb = False
-        self.options["ffmpeg/*"].with_lzma = False
-        self.options["ffmpeg/*"].with_bzip2 = False
-        self.options["ffmpeg/*"].with_pulse = False
-        self.options["ffmpeg/*"].with_vaapi = False
-        self.options["ffmpeg/*"].with_vdpau = False        
-        self.options["ffmpeg/*"].with_vulkan = False        
-        self.options["ffmpeg/*"].with_libx264 = False
-        self.options["ffmpeg/*"].with_libx265 = False
-        self.options["ffmpeg/*"].with_libalsa = False
-        self.options["ffmpeg/*"].with_libiconv = False
-        self.options["ffmpeg/*"].with_freetype = False
-        self.options["ffmpeg/*"].with_libmp3lame = False
-        self.options["ffmpeg/*"].with_libfdk_aac = False
+        #self.options["ffmpeg/*"].with_xcb = False
+        #self.options["ffmpeg/*"].with_lzma = False
+        #self.options["ffmpeg/*"].with_bzip2 = False
+        #self.options["ffmpeg/*"].with_pulse = False
+        #self.options["ffmpeg/*"].with_vaapi = False
+        #self.options["ffmpeg/*"].with_vdpau = False        
+        #self.options["ffmpeg/*"].with_vulkan = False        
+        #self.options["ffmpeg/*"].with_libx264 = False
+        #self.options["ffmpeg/*"].with_libx265 = False
+        #self.options["ffmpeg/*"].with_libalsa = False
+        #self.options["ffmpeg/*"].with_libiconv = False
+        #self.options["ffmpeg/*"].with_freetype = False
+        #self.options["ffmpeg/*"].with_libmp3lame = False
+        #self.options["ffmpeg/*"].with_libfdk_aac = False
         
-        self.options["ffmpeg/*"].with_programs = False
-        self.options["ffmpeg/*"].disable_all_encoders = True
-        self.options["ffmpeg/*"].disable_all_hardware_accelerators = True
+        #self.options["ffmpeg/*"].with_programs = False
+        #self.options["ffmpeg/*"].disable_all_encoders = True
+        #self.options["ffmpeg/*"].disable_all_hardware_accelerators = True
