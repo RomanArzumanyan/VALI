933e12d1265f067bcb54d69943ebbc4111d31cf6
Roman Arzumanyan <roman.arzum@gmail.com>
2024-12-12 16:15:40 +0300

    Trim dead code

--- src/python_vali/src/PyDecoder.cpp
+++ src/python_vali/src/PyDecoder.cpp
@@ -400,12 +400,6 @@ void Init_PyDecoder(py::module& m) {
         :param seek_ctx: seek context, may be None
         :return: tuple, first element is True in case of success, False otherwise. Second elements is TaskExecInfo.
     )pbdoc")
-      .def("TakeBuffer",
-           [](PyDecoder& self, py::object buf) {
-             BufferedReader buffer(buf);
-             std::vector<uint8_t> scrap(128);
-             BufferedReader::read((void*)&buffer, scrap.data(), scrap.size());
-           })
       .def_property_readonly("Width", &PyDecoder::Width,
                              R"pbdoc(
         Return encoded video file width in pixels.
